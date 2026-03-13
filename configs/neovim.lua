8dc16964a094927dce74eb5827ed5b845f44b9fb
Pablo Vergés <hello@pablo.codes>
2026-03-14 00:45:59 +0100

    Migrate most of vimrc to neovim.lua

--- configs/neovim.lua
+++ configs/neovim.lua
@@ -1,9 +1,28 @@
+vim.g.mapleader = ","
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "K", vim.lsp.buf.hover)
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
 vim.lsp.enable({
   "bashls",
   "nixd",
   "pyright",
   "ts_ls",
 })
-vim.keymap.set("n", "gd", vim.lsp.buf.definition)
-vim.keymap.set("n", "K", vim.lsp.buf.hover)
-vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
+vim.opt.hidden = true
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.incsearch = true
+vim.opt.laststatus = 3
+vim.opt.mouse = "r"
+vim.opt.number = true
+vim.opt.ruler = true
+vim.opt.showmatch = true
+vim.opt.showtabline = 2
+vim.opt.smartindent = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.timeoutlen = 50
+vim.opt.whichwrap:append("<,>,h,l")
+vim.opt.wildmenu = true
+vim.opt.wildmode = "longest:full"
+vim.opt.wrap = true
